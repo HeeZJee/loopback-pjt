b4fbb713b4ab836abc66625a9da43273327cd331
HeeZJee <hafeezghanchi927@gmail.com>
2021-03-11 21:06:42 +0500

    added remote method

--- common/models/city.js
+++ common/models/city.js
@@ -1,5 +1,22 @@
 'use strict';
 
-module.exports = function(City) {
+module.exports = function (City) {
+ 
+    City.populate = (count, cb)=>{
+        console.log(count)
+
+            if(count <= 0){
+                const err = {
+                    statusCode: "400",
+                    message: "You need to populate with one or more people"
+                }
+                cb(err)
+            }
+            const result = {
+                status: `Region populated by ${count} people`
+            }
+            cb(null,result)
+    }
 
 };
+  
